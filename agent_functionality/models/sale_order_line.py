fd07a22bb66271dd989860c2926da6bb6a82528a
igor <bujhm1313@gmail.com>
2017-11-11 16:49:44 +0200

    [IMP] Change price_subtotal calculation in sale_order_line

--- agent_functionality/models/sale_order_line.py
+++ agent_functionality/models/sale_order_line.py
@@ -8,5 +8,24 @@ class SaleOrderLineAgent(models.Model):
 
     agent_product = fields.Many2one('agent.product')
 
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id',
+                 'agent_product')
+    def _compute_amount(self):
+        """
+        Override standard Odoo function for computing price_subtotal
+        """
 
+        # Call super function and compute all prices
+        super(SaleOrderLineAgent, self)._compute_amount()
 
+        for record in self:
+            # If there is an agent for the product
+            if record.agent_product:
+                # Calculate discount
+                record_discount = record.agent_product.agent_discount/100.0
+                discount_sum = record.price_subtotal * record_discount
+
+                # Calculate sum - discount sum
+                record.price_subtotal = record.price_subtotal - discount_sum
+            else:
+                pass
