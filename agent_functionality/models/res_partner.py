3680a19d0095618050e20332deaf2124d632f128
igor <bujhm1313@gmail.com>
2017-11-11 14:38:07 +0200

    [IMP] Add One2many on res_partner on new discount agent model

--- agent_functionality/models/res_partner.py
+++ agent_functionality/models/res_partner.py
@@ -7,3 +7,4 @@ class Agent(models.Model):
     _inherit = 'res.partner'
 
     is_agent = fields.Boolean(string='Is agent')
+    agent_products = fields.One2many('agent.product', inverse_name='partner')
