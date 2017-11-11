5a1869435a8dde6fe0b2cd83d9425f5b9a065f57
igor <bujhm1313@gmail.com>
2017-11-11 15:24:53 +0200

    [ADD] Add sale_order_line model inheritance

--- agent_functionality/models/__init__.py
+++ agent_functionality/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import res_partner
 from . import agent_product
+from . import sale_order_line
---
+++ agent_functionality/models/sale_order_line.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class SaleOrderLineAgent(models.Model):
+    _inherit = 'sale.order.line'
+
+    agent_product = fields.Many2one('agent.product')
